dac6482ff7faf8de077090008d40e938df16c747
songzongyao <704140897@qq.com>
2017-10-12 12:26:06 +0800

    填写了基础功能的空函数

--- project/CanToolAPP/app/src/main/java/com/example/songzy/cantoolapp/info_analysis.java
+++ project/CanToolAPP/app/src/main/java/com/example/songzy/cantoolapp/info_analysis.java
@@ -5,4 +5,20 @@ package com.example.songzy.cantoolapp;
  */
 
 public class info_analysis {
+
+    //总的操作流程方法，调用各个方法执行操作，进行字符串解析
+    public String operate(String x){
+        String str_out = null;
+        return str_out;
+    }
+
+    //传入数据的解析，从蓝牙部分传进来字符串，根据编码规则，将字符串分解
+    private String[] strSplit(String str_in){
+        String[] str_arr = new String[4];
+
+        return str_arr;
+    }
+
+    //建立表格，
+
 }
