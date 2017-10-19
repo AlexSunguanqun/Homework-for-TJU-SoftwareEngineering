f0026758f50f68f9f05c12764d37060e24b611cf
Diison <1217426619@qq.com>
2017-10-19 15:11:43 +0800

    database access

---
+++ com/modern/cantool/sqlutils/Message.java
@@ -0,0 +1,26 @@
+package com.model;
+
+public class Message {
+	public int _id;
+	public String bo_;
+	public int id;
+	public String name;
+	public int dlc;
+	public String node_name;
+
+	public MMessage() {
+	
+	}
+
+	public MMessage(int _id, String bo_, int id, String name,
+			int dlc, String node_name) {
+		super();
+		this._id = _id;
+		this.bo_flag = bo_;
+		this.id = id;
+		this.name = name;
+		this.dlc = dlc;
+		this.node_name = node_name;
+	}
+
+}
---
+++ com/modern/cantool/sqlutils/Unit.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class Unit {
+	public String value;
+	public int    index;
+	
+	public MUnit(String value, int index) {
+		super();
+		this.value = value;
+		this.index = index;
+	}
+	
+}
